94c3eaa3148504762c29fe52a19b81b2ad83b81e
milan-taylor <mtailor@oaktreeapps.com>
2023-06-12 17:33:48 +0530

    testcases added and remove extra code

--- server/src/UnitTestUtils/FakeModels.ts
+++ server/src/UnitTestUtils/FakeModels.ts
@@ -1,3 +1,30 @@
+import { faker } from "@faker-js/faker";
+import {
+  AdminUserModel,
+  IAdminUserEntity,
+} from "../Database/Entities/AdminUserEntity";
+import { IUserEntity, UserModel } from "../Database/Entities/UserEntity";
+import { StringUtils } from "../Utils/StringUtils";
 export class FakeModels {
-  // All the Fake Models
+  public adminUser(): IAdminUserEntity {
+    return new (AdminUserModel())({
+      emailId: faker.internet.email(),
+      passwordHash: faker.datatype.string(),
+      userName: faker.internet.userName(),
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.past(),
+    });
+  }
+
+  public user(): IUserEntity {
+    return new (UserModel())({
+      emailId: faker.internet.email(),
+      passwordHash: StringUtils.toBase64String(faker.datatype.string()),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      userName: faker.internet.userName(),
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.past(),
+    });
+  }
 }
